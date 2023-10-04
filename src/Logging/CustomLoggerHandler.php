5fde511ce89e02481772157d7cff5da96504edbc
anbozsultonov <anboz98sultonov@gmail.com>
2023-10-04 09:07:09 +0500

    refactor

--- src/Logging/CustomLoggerHandler.php
+++ src/Logging/CustomLoggerHandler.php
@@ -22,17 +22,18 @@ final class CustomLoggerHandler extends AbstractProcessingHandler
         $log = Log::channel($channelName);
 
         $message = $record['message'];
+        $context = $record['context'];
 
         match ($channelName) {
-            'info' => $log->info($message),
-            'error' => $log->error($message),
-            'warning' => $log->warning($message),
-            'emergency' => $log->emergency($message),
-            'alert' => $log->alert($message),
-            'critical' => $log->critical($message),
-            'notice' => $log->notice($message),
-            'debug' => $log->debug($message),
-            default => $log->info($message)
+            'info' => $log->info($message, $context),
+            'error' => $log->error($message, $context),
+            'warning' => $log->warning($message, $context),
+            'emergency' => $log->emergency($message, $context),
+            'alert' => $log->alert($message, $context),
+            'critical' => $log->critical($message, $context),
+            'notice' => $log->notice($message, $context),
+            'debug' => $log->debug($message, $context),
+            default => $log->info($message, $context)
         };
     }
 }
